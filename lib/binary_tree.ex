fc15f879f5ae7e16003d42090c809ea1635f7258
Gregor Brett <gregor.brett@bluefinsolutions.com>
2017-02-27 18:00:15 +0000

    binary tree coming together

--- lib/binary_tree.ex
+++ lib/binary_tree.ex
@@ -1,20 +1,38 @@
 defmodule BinaryTree do
-  defstruct value: nil, left: nil, right: nil
 
-  def add do
-    IO.puts "ok"
+  defmodule BinaryTree.Node do
+    defstruct value: nil, range: nil, left: nil, right: nil
   end
 
   def biggest_power_of_2_after_n(n) do
-    :math.pow(2, (:math.log2(n) |> Float.ceil))
+    :math.pow(2, (:math.log2(n) |> Float.ceil)) |> round
   end
 
   def create_leaves(n) do
-    0..n
+    for x <- 0..biggest_power_of_2_after_n(n) do
+      %BinaryTree.Node{value: 0, range: x..x, left: nil, right: nil}
+    end
+  end
+
+  def create_tree_from_leaves(tree, true), do: tree |> List.first
+
+  def create_tree_from_leaves(leaves, is_root \\false) do
+    leaves
     |> Enum.chunk(2)
-    |> Enum.map( fn(chunk)->
-      %BinaryTree{value: 0, left: nil, right: nil}
+    |> Enum.map( fn(chunk) ->
+
+      x1..y1 = Enum.at(chunk, 0).range
+      x2..y2 = Enum.at(chunk, 1).range
+
+      %BinaryTree.Node{value: 0, range: x1..y2,
+       left: Enum.at(chunk, 0), right: Enum.at(chunk, 1)}
     end)
+    |> create_tree_from_leaves( length(leaves) == 2 )
+
+  end
+
+  def print_tree(tree) do
+    IO.inspect tree 
   end
 
 end
